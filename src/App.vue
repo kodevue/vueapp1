bf9186072c63a31689e98af0fa14cb7059d9a130
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-06-25 13:15:12 +0530

    test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <h1>Hello World</h1>
+
+  <h1>Hello</h1>
+  <h1>Hello</h1>
+  <HelloWorld />
 </template>
 
 <style scoped>

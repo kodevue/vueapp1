bc14e1423e6ba85663a4ff2d5b222c282cc03a60
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-03 19:25:45 +0530

    scoped styling.

--- src/components/SimpleInjectComp.vue
+++ src/components/SimpleInjectComp.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { inject } from 'vue'
 
+const color = 'white'
+const theme = { color: 'green', padding: '16px' }
 let message = inject('message')
 </script>
 
@@ -8,3 +10,11 @@ let message = inject('message')
   <h1>Inject Component</h1>
   <h2>{{ message }}</h2>
 </template>
+
+<style scoped>
+h1 {
+  color: v-bind(color);
+  background: v-bind('theme.color');
+  padding: v-bind('theme.padding');
+}
+</style>
